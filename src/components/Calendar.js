e17ac97bd88257593e35854e34a5566c6a8b2c55
Christian Ambukita <christianambukita@gmail.com>
2021-12-02 19:26:20 +0100

    Add moon phase title to main scene

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,6 +3,7 @@ import { useEffect, useRef , useState} from "react"
 import calculatePhase from "../utils/phaseCalculator"
 import Moon from './Moon';
 import useWindowSize from "../utils/useWindowSize";
+import { getPhaseName, PHASE_NAMES } from "../utils/getPhaseName";
 
 export default function Calendar (){
     const actPhase = calculatePhase(new Date())
@@ -12,22 +13,9 @@ export default function Calendar (){
     const [year, setYear] = useState()
     const [mArray, setMArray] = useState([])
     const MONTH_NAMES = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    const PHASE_NAMES = ["New Moon", "Waxing crescent", "First quarter", "Waxing gibbous", "Full Moon", "Waning gibbous", "Last quater", "Waning crescent"]
     let size = useWindowSize()
 
-    function getPhaseName(phase, names){
-        const ranges = [0.03, 0.23, 0.27, 0.48, 0.52, 0.73, 0.77, 0.97]
-        //              --0-- --1-- --2-- --3-- --4-- --5-- --6-- --7--
-        if( ranges[7] <= phase || phase <= ranges[0] ) return names[0];
-        if( ranges[0] < phase && phase < ranges[1]) return names[1];
-        if( ranges[1] <= phase && phase <= ranges[2]) return names[2];
-        if( ranges[2] < phase && phase < ranges[3]) return names[3];
-        if( ranges[3] <= phase && phase <= ranges[4]) return names[4];
-        if( ranges[4] < phase && phase < ranges[5]) return names[5];
-        if( ranges[5] <= phase && phase <= ranges[6]) return names[6];
-        if( ranges[6] < phase && phase < ranges[7]) return names[7];
-        console.warn(`Invalid moon phase: ${phase}, expected floating number from range from 0.0 - 1.0`);
-    }
+ 
 
     function getMoonSize({width, height}){
         return Math.floor(width>height? height*0.05 : width*0.06)
--- src/components/MainScene.js
+++ src/components/MainScene.js
@@ -4,13 +4,14 @@ import Moon from './Moon';
 import calculatePhase from "../utils/phaseCalculator"
 import useWindowSize from "../utils/useWindowSize";
 import useScrollHeight from '../utils/useScrollHeight';
-
+import "../css/MainScene.css"
+import { getPhaseName } from "../utils/getPhaseName";
 
 export default function MainScene(){
     let windowSize = useWindowSize();
     //let [sliderValue, setSliderValue] = useState(0.5);
     let scrollPosition = useScrollHeight()
-
+    const actualPhase = calculatePhase(new Date())
     function getMoonSize({width, height}){
         return Math.floor(width>height? height*0.5 : width*0.5)
     }
@@ -19,25 +20,26 @@ export default function MainScene(){
         if(scrollPosition) return `translateY(${scrollPosition/ratio}px)`
         return ''
     }
-
+    console.log(scrollPosition)
     return(
         <>
-        <div className="scene-container">
-            <div className = "stars-bg" style = {{transform: `${getTranslateY(scrollPosition, 5)}`}}></div>
-            <Moon phase={calculatePhase(new Date())} moonSize = {getMoonSize(windowSize)} scrollTranslate = {getTranslateY(scrollPosition, 3)}/>
+            <div className="scene-container">
+                { <h1 className={scrollPosition ? 'opacity-0' : ""}>{getPhaseName(actualPhase)}</h1>}
+                <div className = "stars-bg" style = {{transform: `${getTranslateY(scrollPosition, 5)}`}}></div>
+                <Moon phase={actualPhase} moonSize = {getMoonSize(windowSize)} scrollTranslate = {getTranslateY(scrollPosition, getMoonSize(windowSize)/100 )}/>
 
-            <div className="tree-line">
-                <img src={treeLineImg} alt="tree-line"/>
-            </div>
-            {
-            /*
-            <div className="slidecontainer">
-                <input type="range" min="0" max="1" step="0.01" value={sliderValue} className="slider" id="myRange" onChange={(e)=>setSliderValue(e.target.value)}/>
-            </div>
-            */
-            }
-      </div>
-      <div className = "scene-seam-patch"></div>
+                <div className="tree-line">
+                    <img src={treeLineImg} alt="tree-line"/>
+                </div>
+                {
+                /*
+                <div className="slidecontainer">
+                    <input type="range" min="0" max="1" step="0.01" value={sliderValue} className="slider" id="myRange" onChange={(e)=>setSliderValue(e.target.value)}/>
+                </div>
+                */
+                }
+        </div>
+        <div className = "scene-seam-patch"></div>
       </>
     )
 }
